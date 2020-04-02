87d1c1b233fe47101a0505a58b8050077471b196
Nell3r <Mr.henkay@gmail.com>
2020-04-02 19:49:12 +0300

    Apps commit

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -3,6 +3,7 @@ package tests;
 import model.User;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import pages.*;
 
@@ -32,7 +33,7 @@ public class BaseTest {
         signUpPage = new SignUpPage(driver);
     }
 
-    //@AfterMethod(alwaysRun = true)
+   @AfterMethod(alwaysRun = true)
     public void closeDriver() {
         driver.quit();
     }
